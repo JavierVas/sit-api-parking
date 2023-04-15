be7eace1674475975413d36dd3f685996deaa780
Victor Javier Vasquez Trauco <VVASQUEZT2@UPAO.EDU.PE>
2023-04-15 09:25:37 -0500

    feat: Crear la clase parkingServices. Implemento los metodos getAllCars y addCars

--- api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
+++ api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
@@ -1,4 +1,28 @@
 package com.parking.apiparking.services;
 
+import com.parking.apiparking.entities.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingService {
+
+    private List<Car> parkingLot;
+
+    public ParkingService(){
+        this.parkingLot = new ArrayList<>();
+    }
+
+    //TODO: como usuario, quiero poder ver la lista de todos los autos estacionados en el
+    //parqueo, para poder tener una vision general del estado actual del estacionamiento
+
+    public List<Car> getAllCars(){
+        return this.parkingLot;
+    }
+
+    //TODO: Como usuario, quiero poder agregar un auto al parqueo, para poder estacionar mi vehiculo
+
+    public void addCar(Car Car) {
+        this.parkingLot.add(Car);
+    }
 }
